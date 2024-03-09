ca80e7b346c57c5f609925aa7f83c681abb83890
Akib Bagwan <akibbagwan69@gmail.com>
2024-03-09 11:56:41 +0530

    removed incre decre funcs

--- src/counter.jsx
+++ src/counter.jsx
@@ -7,26 +7,13 @@ class Counter extends React.Component {
       count: 0,
     };
   }
-
-  increment() {
-    this.setState((previousValue) => ({
-      count: previousValue.count + 1,
-    }));
-  }
-
-  decrement() {
-    this.setState((previousValue) => ({
-      count: previousValue.count - 1,
-    }));
-  }
-
   render() {
     return (
       <div className="counter">
         <h1>Counter app</h1>
         <p>Count: {this.state.count}</p>
-        <button onClick={(e) => this.increment(e)}>increment</button>
-        <button onClick={(e) => this.decrement(e)}>decrement</button>
+        <button onClick={(e) => this.setState((prev)=>({ count: prev.count + 1}))}>increment</button>
+        <button onClick={(e) => this.setState((prev)=>({ count: prev.count - 1}))}>decrement</button>
       </div>
     );
   }
